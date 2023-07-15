37a4c35e450690323c1969f31dd86f109a642064
dissna <123608388+dissna@users.noreply.github.com>
2023-07-15 13:49:09 +0300

    🔥 start project

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+from .main import main
+from app.loggers.init_logging import init_logging
---
+++ app/main.py
@@ -0,0 +1,17 @@
+from app.services.convert_file import convert_file
+
+
+def main():
+    # generate_csv_with_users()
+
+    convert_file()
+
+    # show_greeting()
+    # check_file_exists()
+    #
+    # bla1()
+    # bla2(
+    #     foo_1=1,
+    #     #
+    #     foo_3=3,
+    # )
