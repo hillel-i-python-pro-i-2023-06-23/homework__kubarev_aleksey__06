6dee4513216df00adce2aa29301d7bf0d45fc2a6
dissna <123608388+dissna@users.noreply.github.com>
2023-07-15 13:59:33 +0300

    🔥 start project [update]

--- app/loggers/loggers.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def get_core_logger() -> logging.Logger:
-    return logging.getLogger("core")
-
-
-def get_custom_logger(logger_name: str) -> logging.Logger:
-    return logging.getLogger(logger_name)
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from app import main, init_logging
+from app import main
 
 if __name__ == "__main__":
-    init_logging()
     main()
